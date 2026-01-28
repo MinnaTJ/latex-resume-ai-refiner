220b0bc7ee7c9af0571c7773b2f355c6ffeebeb5
MinnaTJ <tjminna@gmail.com>
2026-01-28 22:49:21 +0530

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 const __filename = fileURLToPath(import.meta.url);
